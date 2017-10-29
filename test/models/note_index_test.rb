9219d48564fb58751a62b6e5b2ea6524d2339588
Koichi Yoshizaki <kyoshizaki@gmail.com>
2017-10-29 11:19:23 +0900

    :wrench: Fix snippet creation bug

--- test/models/note_index_test.rb
+++ test/models/note_index_test.rb
@@ -39,12 +39,6 @@ class NoteIndexTest < ActiveSupport::TestCase
     assert_invalid build(:note_index, snippet_id: nil), :snippet_id
   end
 
-  # test for validates_uniqueness_of :display_order, scope: [:note_id]
-  test 'some note_indices with same display_order and note_id are invalid' do
-    note_index = create(:note_index)
-    assert_invalid build(:note_index, display_order: note_index.display_order, note_id: note_index.note_id), :display_order
-  end
-
   # test for validates_uniqueness_of :snippet_id, scope: [:note_id]
   test 'some note_indices with same snippet_id and note_id are invalid' do
     note_index = create(:note_index)
