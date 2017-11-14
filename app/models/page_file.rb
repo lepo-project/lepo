6a89273ac4b264379d7ae54f3e5af0f840a9b286
Junmi Nakashima <j-nakashima@oita-u.ac.jp>
2017-11-14 04:55:37 +0000

    :wrench: Add process to overwrite the existing files. #84

--- app/models/page_file.rb
+++ app/models/page_file.rb
@@ -17,7 +17,7 @@ class PageFile < ApplicationRecord
   has_attached_file :upload,
   path: ':rails_root/public/system/contents/:content_folder_name/:filename',
   url: ':relative_url_root/system/contents/:content_folder_name/:filename'
-  validates_attachment_content_type :upload, content_type: ['image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/x-png', 'text/html', 'video/mp4', 'video/quicktime', 'video/x-flv']
+  validates_attachment_content_type :upload, content_type: ['application/pdf', 'image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/x-png', 'text/html', 'video/mp4', 'video/quicktime', 'video/x-flv']
   validates_attachment_size :upload, less_than: CONTENT_MAX_FILE_SIZE.megabytes
   belongs_to :content, touch: true
   has_many :stickies, as: :target, dependent: :destroy
