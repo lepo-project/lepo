43e2a7a6468600a8684c9fbdf2427edab14150e6
Koichi Yoshizaki <kyoshizaki@gmail.com>
2019-06-19 15:18:17 +0900

    :wrench: Fix the validation notations of sticky to new style

--- test/models/sticky_test.rb
+++ test/models/sticky_test.rb
@@ -29,42 +29,42 @@ class StickyTest < ActiveSupport::TestCase
     assert build(:course_note_sticky).valid?
   end
 
-  # test for validates_absence_of :content_id, if: "target_type == 'Note'"
-  test 'a note sticky with content_id is invalid' do
-    assert_invalid build(:note_sticky, content_id: 1), :content_id
-  end
-
-  # test for validates_presence_of :content_id, if: "target_type == 'Page'"
-  test 'a page sticky without content_id is invalid' do
-    assert_invalid build(:sticky, content_id: ''), :content_id
-    assert_invalid build(:sticky, content_id: nil), :content_id
-  end
-
-  # test for validates_presence_of :course_id, if: "category == 'course'"
-  test 'a course sticky without course_id is invalid' do
-    assert_invalid build(:course_sticky, course_id: ''), :course_id
-    assert_invalid build(:course_sticky, course_id: nil), :course_id
-  end
-
-  # test for validates_presence_of :manager_id
-  test 'a sticky without manager_id is invalid' do
-    assert_invalid build(:sticky, manager_id: ''), :manager_id
-    assert_invalid build(:sticky, manager_id: nil), :manager_id
-  end
-
-  # test for validates_presence_of :target_id
-  test 'a sticky without target_id is invalid' do
-    assert_invalid build(:sticky, target_id: ''), :target_id
-    assert_invalid build(:sticky, target_id: nil), :target_id
-  end
-
-  # test for validates_inclusion_of :category, in: %w[private course]
+  # validates :category, inclusion: { in: %w[private course] }
   test 'a sticky with category that is not incluede in [private course] is invalid' do
     assert_invalid build(:sticky, category: ''), :category
     assert_invalid build(:sticky, category: nil), :category
   end
 
-  # test for validates_inclusion_of :target_type, in: %w[Page Note]
+  # validates :content_id, absence: true, if: "target_type == 'Note'"
+  test 'a note sticky with content_id is invalid' do
+    assert_invalid build(:note_sticky, content_id: 1), :content_id
+  end
+
+  # validates :content_id, presence: true, if: "target_type == 'Page'"
+  test 'a page sticky without content_id is invalid' do
+    assert_invalid build(:sticky, content_id: ''), :content_id
+    assert_invalid build(:sticky, content_id: nil), :content_id
+  end
+
+  # validates :course_id, presence: true, if: "category == 'course'"
+  test 'a course sticky without course_id is invalid' do
+    assert_invalid build(:course_sticky, course_id: ''), :course_id
+    assert_invalid build(:course_sticky, course_id: nil), :course_id
+  end
+
+  # validates :manager_id, presence: true
+  test 'a sticky without manager_id is invalid' do
+    assert_invalid build(:sticky, manager_id: ''), :manager_id
+    assert_invalid build(:sticky, manager_id: nil), :manager_id
+  end
+
+  # validates :target_id, presence: true
+  test 'a sticky without target_id is invalid' do
+    assert_invalid build(:sticky, target_id: ''), :target_id
+    assert_invalid build(:sticky, target_id: nil), :target_id
+  end
+
+  # validates :target_type, inclusion: { in: %w[Page Note] }
   test 'a sticky with target_type that is not incluede in [Page Note] is invalid' do
     assert_invalid build(:sticky, target_type: ''), :target_type
     assert_invalid build(:sticky, target_type: nil), :target_type
